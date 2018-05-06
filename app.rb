b991dbb886f1d02e9097179a102f43b053445e50
Trevor Seitz <trevinla@gmail.com>
2018-05-06 15:50:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   get '/hometown' do
     "My hometown is Calgary."
   end
-  get 'favorite-song' do
+  get '/favorite-song' do
     "My favorite song is different depending on my mood."
   end
 end
